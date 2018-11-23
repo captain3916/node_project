428b74ad2e4ae26be8b71e9d8a159c2f8db6d4b2
captain3916 <guoningdong2@163.com>
2018-11-23 20:52:07 +0800

    抄了1个小时的CSS样式，勉强能看了

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/users', function (req, res, next) {
+  res.render('users');
+});
+
+router.get('/phone', function (req, res, next) {
+  res.render('phone');
+});
+
+router.get('/brand', function (req, res, next) {
+  res.render('brand');
 });
 
 module.exports = router;
